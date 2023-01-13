fbcb0d0e43e77f881064dfcb08ab004e8ff9041b
Dimion <ardgalen@dimlev.ru>
2023-01-13 12:28:36 +0300

    task 02

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine()); //25 
+int number2 = Convert.ToInt32(Console.ReadLine()); // 2
+int square = number2 * number2;
+if (number1 == square )
+{
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
